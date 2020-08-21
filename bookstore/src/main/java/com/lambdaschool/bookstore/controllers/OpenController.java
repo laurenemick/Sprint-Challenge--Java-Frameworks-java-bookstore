fdda5ad2c17662cab5341555df5f6ba69b036b6e
Lauren Emick <laurenemick6@gmail.com>
2020-08-21 09:15:20 -0700

    java.lang.IllegalStateException: Failed to load ApplicationContext error

--- bookstore/src/main/java/com/lambdaschool/bookstore/controllers/OpenController.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/controllers/OpenController.java
@@ -6,7 +6,11 @@ import com.lambdaschool.bookstore.models.UserRoles;
 import com.lambdaschool.bookstore.services.RoleService;
 import com.lambdaschool.bookstore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -90,13 +94,17 @@ public class OpenController
         // return the access token
         // To get the access token, surf to the endpoint /login just as if a client had done this.
         // You cannot use a port when on Heroku
-        String port = "";
-        if (httpServletRequest.getServerName()
-            .equalsIgnoreCase("localhost"))
-        {
-            port = ":" + httpServletRequest.getLocalPort();
-        }
-        String requestURI = "http://" + httpServletRequest.getServerName() + port + "/login";
+//        String port = "";
+//        if (httpServletRequest.getServerName()
+//            .equalsIgnoreCase("localhost"))
+//        {
+//            port = ":" + httpServletRequest.getLocalPort();
+//        }
+//        String requestURI = "http://" + httpServletRequest.getServerName() + port + "/login";
+        RestTemplate restTemplate = new RestTemplate();
+        String requestURI = "http://" + httpServletRequest.getServerName() +
+            (httpServletRequest.getServerName().equalsIgnoreCase("localhost") ? ":" + httpServletRequest.getLocalPort() : "") +
+            "/login";
 
         List<MediaType> acceptableMediaTypes = new ArrayList<>();
         acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
@@ -138,4 +146,5 @@ public class OpenController
     {
 
     }
+
 }
